555f40512c628a578df736a01183741ff7eda13a
Arlmy <arlmy19@gmail.com>
2016-12-28 17:32:14 +0800

    done

--- homework/homework013.js
+++ homework/homework013.js
@@ -1,13 +1,32 @@
-function showPic(whichpic) {
-	var source = whichpic.getAttribute("href");
-	var placeholder = document.getElementById("placeholder");
-	placeholder.setAttribute("src",source);	
-	var text = whichpic.getAttribute("title");
-	var description = document.getElementById("description");
-	description.firstChild.nodeValue = text;
+window.onload = prepareGallery;
+
+function prepareGallery () {
+	if (!document.getElementsByTagName) return false;
+	if (!document.getElementById) return false;
+	if (!document.getElementById("imagegallery")) return false;
+
+	var gallery = document.getElementById("imagegallery");
+	var links = gallery.getElementsByTagName("a");
+
+	for ( var i=0; i < links.length; i++) {
+		links[i].onclick = function() {
+			return showPic(this) ? false : true;
+		}
+	}
 }
 
-function countBodyChildren() {
-	var body_element = document.getElementsByTagName("body")[0];
-	alert(body_element.nodeType);
+function showPic(whichpic) {
+	if (!document.getElementById("placeholder")) return false;
+	var source = whichpic.getAttribute("href");
+	var placeholder = document.getElementById("placeholder");
+	if (placeholder.nodeName != "IMG") return false;
+	placeholder.setAttribute("src",source);
+	if (document.getElementById("description")) {
+		var text = whichpic.getAttribute("title") ? whichpic.getAttribute("title") : "";
+		var description = document.getElementById("description");
+		if (description.firstChild.nodeType == 3) {
+			description.firstChild.nodeValue = text;
+		}
+	}
+	return true;
 }
