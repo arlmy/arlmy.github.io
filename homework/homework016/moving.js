e539433d2292c47a50fbe7e7772d77b9d275f749
arlmy <arlmy19@gmail.com>
2017-01-22 13:24:28 +0800

    homework016 part 2

---
+++ homework/homework016/moving.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+  var canvas = document.getElementById('canvas');
+/*  canvas.width = screen.width;
+  canvas.height = screen.height; 屏幕区域 */
+/*  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight; 可视区域 */
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
