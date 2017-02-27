aff03e5acd9ebebeccf786ead0f4c173b7ea266d
arlmy <arlmy19@gmail.com>
2017-02-27 14:27:55 +0800

    game of life v0.03

--- homework/homework017/life.js
+++ homework/homework017/life.js
@@ -12,6 +12,9 @@ window.onload = function() {
 
 /* 初始网格 */
 function drawLines(canvas, context) {
+  var canvas = document.getElementById("canvas");
+  var context = canvas.getContext("2d");
+  context.fillStyle="#000000";
   var positionX = [];
     for(var i=0; i<30; i++) {
       positionX[i] = i*20;
@@ -20,7 +23,6 @@ function drawLines(canvas, context) {
     for(var j=0; j<30; j++) {
       positionY[j] = j*20;
     }
-
   for(var k=0; k<30; k++) {
     for(var l=0; l<30; l++) {
       context.fillRect(positionX[k],positionY[l],20,20);
@@ -29,39 +31,80 @@ function drawLines(canvas, context) {
   }
 }
 
-/* 初始生命赋值 */
-function giveLife() {
-  var life = new Array();
+/* 按数组填充 */
+function displayLife(life) {
+  for(var i=1; i<29; i++) {
+    for(var j=1; j<29; j++) {
+      if (life[i][j] == 1) {
+        // console.log(life[i][j]);
+        var canvas = document.getElementById("canvas");
+        var context = canvas.getContext("2d");
+        context.fillStyle="#FF0000";
+        context.fillRect(i*20+1,j*20+1,18,18);
+      } else if (life[i][j] == 0) {
+        var canvas = document.getElementById("canvas");
+        var context = canvas.getContext("2d");
+        context.fillStyle="#FFFFFF";
+        context.fillRect(i*20+1,j*20+1,18,18);
+      }
+    }
+  }
+}
+
+var generationCount = 0;
+var firstGeneration = new Array();
     for(var i=0; i<30; i++) {
-      life[i] = new Array();
+        firstGeneration[i] = new Array(); // 格式化
+          for(var j=0; j<30; j++) {
+              firstGeneration[i][j] = 0;
+          }
+    }
+
+/* 初始生命赋值及显示 */
+function giveLife() {
+    for(var i=0; i<30; i++) {
+      firstGeneration[i] = new Array();
         for(var j=0; j<30; j++) {
-          life[i][j] = Math.round(Math.random());
+          firstGeneration[i][j] = Math.round(Math.random()); // 0与1 随机赋值
           // console.log(life[i][j]);
         }
     }
-  console.log(life[1]);
-  newLife(life);
-}
-
-/* 初始生命显示 */
-function newLife(life) {
-  //console.log(life[1]); //传递效果
-  displayLife(life);
-  //lifeEvolution(life);
-
-  // setInterval(lifeEvolution(life),300);
+  // console.log(life[1]);
+  displayLife(firstGeneration);
+  // lifeEvolution(life);
+  generationCount++;
+  // console.log(generationCount);
 }
 
 /* 演绎后代 */
 function displayNextGeneration() {
-    lifeEvolution(giveLife());
+    if ( generationCount == 0 ) {
+        alert("请先点击一次初始生命！");
+    } else if ( generationCount == 1 ) {
+        displayLife(lifeEvolution(firstGeneration));
+    } else {
+        displayLife(lifeEvolution(nextGeneration));
+    }
 }
 
 /* 生命演绎函数，数组赋值 */
+var nextGeneration = new Array();
+    for(var i=0; i<30; i++) {
+        nextGeneration[i] = new Array(); // 格式化
+          for(var j=0; j<30; j++) {
+              nextGeneration[i][j] = 0;
+          }
+    }
+
 function lifeEvolution(life) {
-  var nextGeneration = new Array();
+    for(var i=0; i<30; i++) {
+        nextGeneration[i][0] = 0;
+    }
+    for(var j=0; j<30; j++) {
+        nextGeneration[0][j] = 0;
+    }
+    // console.log(life[1]);
     for(var i=1; i<29; i++) {
-        nextGeneration[i] = new Array();
         for(var j=1; j<29; j++) {
         var count = life[i-1][j-1] + life[i][j-1] + life[i+1][j-1] + life[i-1][j] + life[i+1][j] + life[i-1][j+1] + life[i][j+1] + life[i+1][j+1];
         // console.log(count);
@@ -75,31 +118,89 @@ function lifeEvolution(life) {
             // console.log(nextGeneration[i][j]);
         }
     }
+
   // console.log(nextGeneration[1]); // 测试赋值结果
-  displayLife(nextGeneration);
+  generationCount++;
+  console.log(generationCount);
+  // console.log(nextGeneration[1]);
+  return nextGeneration;
 }
 
-// setInterval(displayLife(),200)
-
-/* 按数组填充 */
-function displayLife(life) {
-  for(var i=1; i<29; i++) {
-    for(var j=1; j<29; j++) {
-      if (life[i][j] == 1) {
-        console.log(life[i][j]);
-        var canvas = document.getElementById("canvas");
-        var context = canvas.getContext("2d");
-        context.fillStyle="#FF0000";
-        context.fillRect(i*20+1,j*20+1,18,18);
-      }
+/* 自动循环 */
+function autoGenerate() {
+    if ( generationCount == 0 ) {
+        giveLife();
+        displayLife(lifeEvolution(firstGeneration));
+        setInterval(function() {
+            displayLife(lifeEvolution(nextGeneration));
+        }, 30);
+    } else {
+        displayLife(lifeEvolution(firstGeneration));
+        setInterval(function() {
+            displayLife(lifeEvolution(nextGeneration));
+        }, 30);
     }
-  }
 }
 
-/*
-
-
-function update_canvas() {
-  console.log("update once!");
+/* 10x10 初始生命 */
+function giveTenLife() {
+    for(var i=10; i<20; i++) {
+      firstGeneration[i] = new Array();
+        for(var j=10; j<20; j++) {
+          firstGeneration[i][j] = Math.round(Math.random()); // 0与1 随机赋值
+        }
+    }
+  displayLife(firstGeneration);
+  generationCount++;
 }
-*/
+
+/* 16x16 初始生命 */
+function giveSixteenLife() {
+    for(var i=7; i<23; i++) {
+      firstGeneration[i] = new Array();
+        for(var j=7; j<23; j++) {
+          firstGeneration[i][j] = Math.round(Math.random()); // 0与1 随机赋值
+        }
+    }
+  displayLife(firstGeneration);
+  generationCount++;
+}
+
+/* Glider/滑翔者 */
+function glider() {
+    firstGeneration = [
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    ];
+  displayLife(firstGeneration);
+  generationCount++;
+  console.log(generationCount);
+}
